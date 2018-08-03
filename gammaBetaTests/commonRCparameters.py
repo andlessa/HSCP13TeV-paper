06d8b56e9270fbcb0acb54f43f30cef70a2ec748
Andre Lessa <lessa.a.p@gmail.com>
2018-08-03 05:45:02 -0300

    Improved gammabeta plot

---
+++ gammaBetaTests/commonRCparameters.py
@@ -0,0 +1,10 @@
+params = {'xtick.labelsize' : 22,
+        'ytick.labelsize' : 22, 
+        'axes.labelsize' : 28, 
+        'figure.figsize' : (15,10),
+         'lines.markersize' : 15,
+         'lines.linewidth' : 3,
+         'legend.fontsize' : 20,
+         'legend.framealpha': 1.0,
+         'font.family' : 'Latin Modern Roman'}
+
